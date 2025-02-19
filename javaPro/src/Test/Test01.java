7c05933b9208b6b0e76a3df7db6820ad49f819df
jin uk <storywlsdnr@naver.com>
2025-02-19 17:37:26 +0900

    333

--- javaPro/src/Test/Test01.java
+++ javaPro/src/Test/Test01.java
@@ -1,15 +1,20 @@
 package Test;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
-
+/**
+ * @author jaewon
+ * @date 2025. 2. 19. -  오전 11:21:10
+ * @subject 
+ * @content
+ */
 public class Test01 {
 
 	public static void main(String[] args) {
 		
 
 
-	}
 
-}
+	} // main
+	
+
+
+} // class
